8d41eb2f89493cbddf7e00aab929b0cc799f0f35
Muhammad Ahmed <Muhammad_Ahmed@HomeDepot.com>
2019-11-08 15:31:59 -0600

    ArmStrong Challenge Completed

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,23 @@
 class ArmstrongNumbers {
+	private int numberToCheck;
 
 	boolean isArmstrongNumber(int numberToCheck) {
+		// Get the String version of the number
+		String stringVersionOfNumber = String.valueOf(numberToCheck);
+		double sum = 0;
+		boolean check;
+		for (int i = 0; i<stringVersionOfNumber.length(); i++){
+			sum = sum + Math.pow(Character.getNumericValue(stringVersionOfNumber.charAt(i)), stringVersionOfNumber.length());
 
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-		
+		}
+
+		if (sum == numberToCheck){
+			check = true;
+		}else{
+			check = false;
+		}
+//		char [] cArray = stringVersionOfNumber.toCharArray();
+//		int number = Integer.parseInt(new String (cArray));
+		return check;
 	}
-
 }
