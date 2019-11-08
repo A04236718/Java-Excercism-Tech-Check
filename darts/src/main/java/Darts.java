848a9f3d640d61452933497569574a3d42d0d906
Muhammad Ahmed <Muhammad_Ahmed@HomeDepot.com>
2019-11-08 12:04:40 -0600

    Darts Completed

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,11 +1,28 @@
+import java.lang.Math;
 class Darts {
-
+    private double x;
+    private double y;
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.x = x;
+        this.y = y;
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        double radiusSqaured = Math.pow(x,2) + Math.pow(y,2);
+        double radius = Math.sqrt(radiusSqaured);
+        int points;
+
+        if ((radius >=0) && (radius <=1)){
+            points = 10;
+        }else if ((radius >1) && (radius <=5)){
+            points = 5;
+        }else if ((radius >5) && (radius <=10)){
+            points = 1;
+        }else{
+            points = 0;
+        }
+
+        return points;
     }
 
 }
